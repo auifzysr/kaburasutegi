95a981481864731dc643cb257da99b3132448471
auifzysr <auifzysr@gmail.com>
2024-06-24 20:14:41 +0900

    parameterise deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-gcloud functions deploy callback \
+gcloud functions deploy $FUNCTION_NAME \
     --gen2 \
-    --region asia-northeast1 \
-    --entry-point Entrypoint \
+    --region $DEFAULT_REGION \
+    --entry-point $FUNCTION_ENTRYPOINT \
     --runtime go122 \
     --trigger-http \
     --memory 128Mi \
     --cpu 0.083 \
     --timeout 30 \
     --max-instances 1 \
-    --allow-unauthenticated
+    --allow-unauthenticated \
+    --run-service-account=$FUNCTION_RUNNER_SERVICE_ACCOUNT_EMAIL \
+    --set-secrets=LINE_CHANNEL_TOKEN=$LINE_CHANNEL_TOKEN_SECRET_ID,LINE_CHANNEL_SECRET=$LINE_CHANNEL_SECRET_SECRET_ID
